1c5af5226384eaafecf52fc852d04579ae238857
Ren <Rochwidias@gmail.com>
2026-07-30 03:16:38 +0700

    fix: tambah web_creator & player_skip bypass

--- bot.py
+++ bot.py
@@ -426,7 +426,8 @@ ytdl_format_options = {
     'default_search': 'auto',
     'source_address': '0.0.0.0',
     'geo_bypass': True,
-    'extractor_args': {'youtube': {'player_client': ['android_creator', 'android_music', 'android', 'tv_embedded', 'ios']}},
+    'force_ipv4': True,
+    'extractor_args': {'youtube': {'player_client': ['android_creator', 'android_music', 'android', 'tv_embedded', 'ios', 'web_creator'], 'player_skip': ['webpage', 'configs']}},
     'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
 }
 COOKIES_FILE = os.path.join(BASE_DIR, "youtube_cookies.txt")
